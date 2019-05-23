efc36e3634006cfed89be7ceb4a9eed0be3dd1fc
antoine@fitle.com <antoine@fitle.com>
2019-05-23 11:51:58 +0200

    Add multi-stage deployment and SSH key option for private repos

--- scikit_deploy/build.py
+++ scikit_deploy/build.py
@@ -7,6 +7,7 @@ import os.path as osp
 import docker
 import json
 import pickle
+from typing import Optional
 
 logger = logging.getLogger("scikit-deploy")
 
@@ -32,30 +33,45 @@ def _prepare_workspace(temp_dir, config, requirements_path):
     Prepares the temporary workspace directory for the docker build
     """
     # copy the server package as the workspace directory
-    shutil.copytree(pkg_resources.resource_filename(
-        __name__, "scikit_deploy_server"), osp.join(temp_dir, "workspace"))
+    shutil.copytree(
+        pkg_resources.resource_filename(__name__, "scikit_deploy_server"),
+        osp.join(temp_dir, "workspace"),
+    )
 
     # copy resources (clf, config) to the workspace server resources
     resources_folder = osp.join(temp_dir, "workspace", "server", "resources")
-    json.dump(config, open(osp.join(resources_folder, "config.json"), 'w'))
-    for endpoint in config['endpoints']:
-        shutil.copy(endpoint['model_path'], osp.join(
-            resources_folder, endpoint['model_name']))
+    json.dump(config, open(osp.join(resources_folder, "config.json"), "w"))
+    for endpoint in config["endpoints"]:
+        shutil.copy(
+            endpoint["model_path"], osp.join(resources_folder, endpoint["model_name"])
+        )
     if requirements_path is not None:
         _prepare_requirements(temp_dir, requirements_path)
 
 
-def _build_docker(temp_dir, image_tag):
+def _build_docker(temp_dir, image_tag, ssh_key: Optional[str] = None):
     path = osp.join(temp_dir, "workspace")
     client = docker.APIClient()
     try:
-        output = client.build(
-            path=path,
-            dockerfile="Dockerfile",
-            tag=image_tag,
-            quiet=False,
-            network_mode="host"
-        )
+        if ssh_key is None:
+            output = client.build(
+                path=path,
+                dockerfile="Dockerfile",
+                tag=image_tag,
+                quiet=False,
+                network_mode="host",
+            )
+        else:
+            output = client.build(
+                path=path,
+                dockerfile="Dockerfile-multi-stage",
+                tag=image_tag,
+                quiet=False,
+                network_mode="host",
+                forcerm=True,  # ensure SSH keys don't persist
+                target="multi-stage-build-final",
+                buildargs=dict(SSH_PRIVATE_KEY=ssh_key),
+            )
         for l in output:
             logs = l.decode("utf-8").split("\r\n")
             for line in logs:
@@ -80,13 +96,13 @@ def _validate_route(route: str):
 
 
 def _generate_request(config):
-    endpoint = config['endpoints'][0]
-    route = endpoint['route']
+    endpoint = config["endpoints"][0]
+    route = endpoint["route"]
     qs = "&".join([f"{o['name']}=0" for o in endpoint["inputs"]])
     return f"http://localhost:8000{route}?{qs}"
 
 
-def build(config_path, requirements_path):
+def build(config_path, requirements_path, ssh_key: Optional[str] = None):
     """
     Builds the docker image
     """
@@ -103,11 +119,12 @@ def build(config_path, requirements_path):
                 logger.error("No endpoints specified in config")
                 exit(1)
             for endpoint in config["endpoints"]:
-                _validate_route(endpoint.get('route'))
-                endpoint['model_name'] = endpoint['route'].replace(
-                    '/', '_') + '_clf.pkl'
+                _validate_route(endpoint.get("route"))
+                endpoint["model_name"] = (
+                    endpoint["route"].replace("/", "_") + "_clf.pkl"
+                )
             _prepare_workspace(temp_dir, config, requirements_path)
-            _build_docker(temp_dir, image_tag)
+            _build_docker(temp_dir, image_tag, ssh_key)
             logger.info(f"Successfully built image {image_tag}")
             logger.info("To test, run :")
             logger.info(f"   docker run -p 8000:8000 {image_tag}")

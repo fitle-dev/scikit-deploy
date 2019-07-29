9302208b3a72a7b0bce694e0403108a6c9b1050a
Emile <emile@fitle.com>
2019-07-29 10:42:02 +0200

    WIP

--- scikit_deploy/scikit_deploy_server/server/endpoint.py
+++ scikit_deploy/scikit_deploy_server/server/endpoint.py
@@ -21,7 +21,7 @@ class Endpoint:
         except ValueError:
             message = f"Input could not be coerced to float: {input_info} = {value}"
             LOGGER.error(message)
-            raise APIError(message)
+            raise APIError(message, 400)
 
     def _denormalize_output(self, value: Union[float, np.float], output_info: dict):
         return (to_float(value) * output_info.get("scaling", 1)) + output_info.get(
--- scikit_deploy/scikit_deploy_server/server/prediction.py
+++ scikit_deploy/scikit_deploy_server/server/prediction.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def predict(model, input_data, config):
-    sample = config.process_input(input_data)
-    vec = np.array(sample).reshape(1, -1)
-    res = model.predict(vec)
-    return config.process_output(res)
+    # sample = config.process_input(input_data)
+    # vec = np.array(sample).reshape(1, -1)
+    res = model.predict(input_data)
+    return res
+    # return config.process_output(res)

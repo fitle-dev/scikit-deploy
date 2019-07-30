3d493022c11d36ade6e99455bdcf141ef9722175
acouderc <couderc.antoine@gmail.com>
2019-07-30 15:49:04 +0200

    Update scikit_deploy/scikit_deploy_server/server/prediction.py
    Co-Authored-By: Emile Bres <emile.bres@gmail.com>

--- scikit_deploy/scikit_deploy_server/server/prediction.py
+++ scikit_deploy/scikit_deploy_server/server/prediction.py
@@ -15,4 +15,4 @@ def predict(model, input_data:Union[Dict, ImmutableMultiDict], config:Endpoint):
     sample = config.process_input(input_data)
     vec = np.array(sample).reshape(1, -1)
     res = model.predict(vec)
-    return config.process_output(vec)
+    return config.process_output(res)
